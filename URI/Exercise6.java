c42d2ea1d477b902d44863bfdd67b69a48b856db
Noemi Gabrielly <68128322+NoemiGabis64@users.noreply.github.com>
2022-01-18 17:00:34 +0100

    Ready

---
+++ URI/Exercise6.java
@@ -0,0 +1,32 @@
+/**Read four integer values named A, B, C and D. 
+ * Calculate and print the difference of product A and B by the product of C and D (A * B - C * D). */
+
+package URI;
+
+import java.util.Scanner;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        int A = 0 ;
+        int B = 0;
+        int C = 0;
+        int D = 0;
+        int DIFERENCA = 0;
+
+        A = input.nextInt();
+        B = input.nextInt();
+        C = input.nextInt();
+        D = input.nextInt();
+
+        DIFERENCA = (A*B-C*D);
+
+        System.out.println("DIFERENCA = " + DIFERENCA);
+        
+    }
+        
+}
+    
+
