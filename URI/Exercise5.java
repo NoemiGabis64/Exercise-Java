1f836febd9b7f0dc3ae9a42060130cb938d455e7
Noemi Gabrielly <68128322+NoemiGabis64@users.noreply.github.com>
2022-01-18 14:05:49 +0100

    Ready

--- URI/Exercise5.java
+++ URI/Exercise5.java
@@ -1,3 +1,5 @@
+/**Read three values (variables A, B and C), which are the three student's grades. Then, calculate the average, considering that grade A has weight 2, grade B has weight 3 and the grade C has weight 5. 
+ * Consider that each grade can go from 0 to 10.0, always with one decimal place. */
 package URI;
 public class Exercise5 {
     public static void main(String[] args){
