d05a0e5618e575215d5c80ae4b8572d5978601f8
Noemi Gabrielly <68128322+NoemiGabis64@users.noreply.github.com>
2022-05-08 10:37:22 +0200

    Update Main.java

--- URI/Builders/People/Main.java
+++ URI/Builders/People/Main.java
@@ -6,7 +6,7 @@ public class Main{
             int vetor[] = {};
                 
         pessoas p1 = new pessoas ("Fernanda", 23 , 73.5, 1.80, "Feminino");  
-		pessoas  p2 = new pessoas("Julia", 45, 68.7, 1.75, "Feminino");  
+	pessoas p2 = new pessoas("Julia", 45, 68.7, 1.75, "Feminino");  
         pessoas p3 = new pessoas("Maria", 19, 63.3, 1.73, "Feminino");  
         pessoas p4 = new  pessoas("João", 05 48.2,1.57, "Masculino");  
         pessoas p5 = new pessoas ("Lucas", 12, 56.0,1.65, "Masculino");  
